7b8f7137d05e18c283ead348a135a8e86608f8dd
Charlton-C <user10219@protonmail.com>
2023-12-28 23:21:23 +0300

    Change length and look of song previews, add song preview that are created dynamically

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var expandLibraryButton = document.querySelector("#music_side-library");
 var expandSongsButton = document.querySelector(".expand-songs-div");
+var previewSongsDiv = document.querySelector(".songs");
 
 
 expandLibraryButton.addEventListener("click", () => {
@@ -16,3 +17,30 @@ expandSongsButton.addEventListener("click", () => {
 	expandLibraryButton.style.color = "rgb(106, 107, 111)";
 	expandLibraryButton.style.textDecoration = "none";
 });
+
+
+function createSongsPreview(){
+	var liElement = document.createElement("li");
+	var liElementDiv = document.createElement("div");
+	liElementDiv.setAttribute("class", "img_play");
+	var liElementDivImg = document.createElement("img");
+	liElementDivImg.setAttribute("src", "/resources/drifting.jpg");
+	var liElementDivI = document.createElement("i");
+	liElementDivI.setAttribute("class", "bi playListPlay bi-play-circle-fill");
+	liElementDivI.setAttribute("id", "2");
+	liElementDiv.appendChild(liElementDivImg);
+	liElementDiv.appendChild(liElementDivI);
+	var liElementH5 = document.createElement("h5");
+	liElementH5.setAttribute("class", "song-1-song-name");
+	liElementH5.innerText = "Song name";
+	var br = document.createElement("br");
+	var liElementH5Div = document.createElement("div");
+	liElementH5Div.setAttribute("class", "subtitle song-1-artist-name");
+	liElementH5Div.innerText = "Song artist";
+	liElementH5.appendChild(br);
+	liElementH5.appendChild(liElementH5Div);
+	liElement.appendChild(liElementDiv);
+	liElement.appendChild(liElementH5);
+	previewSongsDiv.appendChild(liElement);
+}
+createSongsPreview();
