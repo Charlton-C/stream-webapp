81005e6f2a3e304f5ad42a910e949bcee9fa517e
Charlton-C <user10219@protonmail.com>
2024-01-20 00:18:21 +0300

    Add updateSongAudioInfo.py to update artistsInfo.json

--- updateSongAudioInfo/updateSongAudioInfo.py
+++ updateSongAudioInfo/updateSongAudioInfo.py
@@ -11,9 +11,11 @@ parentFileDirectory = os.path.dirname(currentFileDirectory)
 # Get the next song to be added to the songsInfo json file
 startingSongNumber = (int(open(parentFileDirectory+"/songAudioInfo/txt/nextSongUpdateFileNumber.txt", "r").readline()))
 
+
 # Input for the number of new songs information to be added
 print("Please enter how many songs you want to add")
 numberOfSongsToAdd = int(input())
+
 
 # For loop to add the new songs info
 for i in range(numberOfSongsToAdd):
@@ -31,7 +33,6 @@ for i in range(numberOfSongsToAdd):
 		songInfo = json.load(open(parentFileDirectory+"/songAudioInfo/json/songsInfo.json"))
 	else:
 		None
-	
 
 	# Load the song metadata
 	try:
@@ -43,6 +44,7 @@ for i in range(numberOfSongsToAdd):
 	songName = song.title
 	songArtist = song.artist
 	songAlbum = song.album
+	songAlbumArtist = song.albumartist
 
 	# Add the song information to songInfo dictionary
 	songInfo[str(startingSongNumber+i)] = [songName, songArtist, songAlbum]
@@ -64,8 +66,6 @@ for i in range(numberOfSongsToAdd):
 	else:
 		None
 
-	songAlbum = song.album
-	songAlbumArtist = song.albumartist
 
 	if songAlbum not in albumInfo.keys():
 		albumInfo[songAlbum] = [songAlbumArtist, [startingSongNumber+i]]
@@ -76,7 +76,48 @@ for i in range(numberOfSongsToAdd):
 		albumInfoJson = json.dumps(albumInfo)
 		open(parentFileDirectory+"/songAudioInfo/json/albumsInfo.json", "w").write(albumInfoJson)
 	else:
-		None 
+		None
+
+
+	# # Update Artist info
+
+
+	# When the artistsInfo.json file is empty, create an empty dictionary
+	if os.stat(parentFileDirectory+"/songAudioInfo/json/artistsInfo.json").st_size == 0:
+		artistInfo = {}
+	# When the artistsInfo.json file is not empty, load the values into a dictionary
+	elif os.stat(parentFileDirectory+"/songAudioInfo/json/artistsInfo.json").st_size != 0:
+		artistInfo = json.load(open(parentFileDirectory+"/songAudioInfo/json/artistsInfo.json", "r"))
+	else:
+		None
+
+
+	if songArtist == songAlbumArtist:
+		if songArtist not in artistInfo.keys():
+			artistInfo[songArtist] = [[songAlbum], [startingSongNumber+i]]
+			artistInfoJson = json.dumps(artistInfo)
+			open(parentFileDirectory+"/songAudioInfo/json/artistsInfo.json", "w").write(artistInfoJson)
+		elif songArtist in artistInfo.keys():
+			if songAlbum not in artistInfo[songArtist][0]:
+				artistInfo[songArtist][0].append(songAlbum)
+			artistInfo[songArtist][1].append(startingSongNumber+i)
+			artistInfoJson = json.dumps(artistInfo)
+			open(parentFileDirectory+"/songAudioInfo/json/artistsInfo.json", "w").write(artistInfoJson)
+		else:
+			None
+	elif songArtist != songAlbumArtist:
+		if songArtist not in artistInfo.keys():
+			artistInfo[songArtist] = [[], [startingSongNumber+i]]
+			artistInfoJson = json.dumps(artistInfo)
+			open(parentFileDirectory+"/songAudioInfo/json/artistsInfo.json", "w").write(artistInfoJson)
+		elif songArtist in artistInfo.keys():
+			artistInfo[songArtist][1].append(startingSongNumber+i)
+			artistInfoJson = json.dumps(artistInfo)
+			open(parentFileDirectory+"/songAudioInfo/json/artistsInfo.json", "w").write(artistInfoJson)
+		else:
+			None
+	else:
+		None
 
 
 # Update the number of the next song to be added to the songsInfo json file number
