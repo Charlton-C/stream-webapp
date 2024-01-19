c0f55d3ce1fb0dec6f5d116b9dbd81d228e77007
Charlton-C <user10219@protonmail.com>
2024-01-19 17:47:34 +0300

    Update the number of the last song whose info got added to the songsInfo.json file

--- updateSongAudioInfo/updateSongInfo.py
+++ updateSongAudioInfo/updateSongInfo.py
@@ -9,9 +9,15 @@ parentFileDirectory = os.path.dirname(currentFileDirectory)
 
 
 # Get the most recent song added to the songsInfo json file
-startingSongNumber = (int(open(parentFileDirectory+"/songAudioInfo/txt/lastUpdateFileNumber.txt", "r").readline()))+1
+startingSongNumber = (int(open(parentFileDirectory+"/songAudioInfo/txt/lastUpdateFileNumber.txt", "r").readline()))
 
+# Input for the number of new songs info to be added
 print("Please enter how many songs you want to add")
-endingSongNumber = int(input())
-for i in range(endingSongNumber):
-	print(startingSongNumber+i)
+numberOfSongsToAdd = int(input())
+
+# For loop to add the new songs info
+for i in range(numberOfSongsToAdd):
+	print((startingSongNumber+1) + i)
+
+# Update the most recent song added to the songsInfo json file number
+open(parentFileDirectory+"/songAudioInfo/txt/lastUpdateFileNumber.txt", "w").write(str(startingSongNumber+numberOfSongsToAdd))
